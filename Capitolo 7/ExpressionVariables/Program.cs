a88d6a97bcf50cddf5e46b0b3cc83812d3fe2d30
zetanove <supportoinformatico@confimprenditori.it>
2019-03-29 17:57:09 +0100

    expression variables

--- Capitolo 7/ExpressionVariables/Program.cs
+++ Capitolo 7/ExpressionVariables/Program.cs
@@ -14,6 +14,8 @@ namespace ExpressionVariables
 
             var r = from s in strings
                     select int.TryParse(s, out var i);
+
+
         }
     }
 
@@ -24,7 +26,9 @@ namespace ExpressionVariables
         {
         }
 
-        public static int Magic = int.TryParse("123", out var i) ? i : 0;
+        private int number = int.TryParse("123", out var i) ? i : 0;
+
+        public int MyNumber { get; set; } = int.TryParse("123", out int n) ? n : 0;
     }
 
     public class B : A
@@ -32,9 +36,15 @@ namespace ExpressionVariables
         public B(string s)
             : base(int.TryParse(s, out var i) ? i : 0)
         {
+            
         }
 
-        public B(int i): base()
+        public B(int i): base(i)
+        {
+
+        }
+
+        public B(object obj) : this(obj is int i? i: 0)
         {
 
         }
