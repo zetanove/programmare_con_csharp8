323ba0baf354faa2cfa4658e3e9c2420281e4da1
zetanove <supportoinformatico@confimprenditori.it>
2019-05-10 16:58:21 +0200

    acceso a registro file system

--- Capitolo 16/FileSystem/Program.cs
+++ Capitolo 16/FileSystem/Program.cs
@@ -23,6 +23,44 @@ namespace FileSystem
                 }
             }
 
+
+            string dirname = @"C:\temp";
+            string filename = "file.txt";
+            string fullpath = Path.Combine(dirname, filename); //C:\\temp\\file.txt"
+            string xml = Path.ChangeExtension(filename, "xml");// file.xml
+            string dir = Path.GetDirectoryName(fullpath); // c:\temp
+            string ext = Path.GetExtension(fullpath); // .txt
+            string file = Path.GetFileName(fullpath); // file.txt
+            string filewithoutext = Path.GetFileNameWithoutExtension(fullpath); // file
+
+            Directory.SetCurrentDirectory("c:\\windows"); //imposta la directory di lavoro corrente
+            string full = Path.GetFullPath(filename); // c:\windows\file.txt
+
+            string root = Path.GetPathRoot(fullpath); // C:
+            bool hasExt = Path.HasExtension("file.txt"); // true
+            bool pathRooted = Path.IsPathRooted("c:\\file.txt"); //true
+
+            string randomFile = Path.GetRandomFileName(); // v3ybhjqf.0xd
+            string tempFile = Path.GetTempFileName(); // C:\Users\UserName\AppData\Local\Temp\tmp210E.tmp
+            string tempPath = Path.GetTempPath(); // C:\Users\UserName\AppData\Local\Temp
+
+            string desktopPath = Environment.GetFolderPath(Environment.SpecialFolder.Desktop);
+
+
+            //file e directory
+            DirectoryInfo tempDir = new DirectoryInfo("c:\\temp\\tempdir");
+            if (tempDir.Exists)
+                tempDir.Delete(true);
+
+            if (Directory.Exists("c:\\temp\\tempdir"))
+                Directory.Delete("c:\\temp\\tempdir", true);
+
+
+
+            bool exists = File.Exists(fullpath);
+            FileInfo fi1 = new FileInfo(fullpath);
+            exists = fi1.Exists;
+
         }
     }
 }
