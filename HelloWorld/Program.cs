0503fd128177f95f0817408fd3e4ce43d73050bc
zetanove <supportoinformatico@confimprenditori.it>
2019-05-13 17:15:33 +0200

    hello world

--- HelloWorld/Program.cs
+++ HelloWorld/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Runtime.InteropServices;
 
 namespace HelloWorld
 {
@@ -17,7 +18,14 @@ namespace HelloWorld
             int[] a = { 0, 1, 2, 3, 4, 5, 6, 7, 8, 9 };
             Console.WriteLine($"{a[i1]}, {a[i2]}"); // "3, 6"
 
-            Console.Read();
+
+            
+
+
         }
     }
+
+
+    
+
 }
