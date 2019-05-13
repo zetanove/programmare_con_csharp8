0929ee3792391f34270114663548ea393b6cb406
zetanove <supportoinformatico@confimprenditori.it>
2019-05-13 17:51:50 +0200

    disconnected ado netcore

---
+++ Capitolo 16/DisconnectedADO_NetCore/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Data;
+using System.Data.SqlClient;
+
+namespace DisconnectedADO_NetCore
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string connString = "Server=.;Integrated Security=true;database=WideWorldImporters";
+
+            DataSet ds = new DataSet();
+            using (SqlConnection conn = new SqlConnection(connString))
+            {
+                conn.Open();
+                SqlDataAdapter adapter = new SqlDataAdapter("SELECT * FROM Sales.Orders", conn);
+                adapter.Fill(ds, "Products");
+            }
+
+        }
+    }
+}
---
+++ Capitolo 16/EF_fw_Console/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EF_fw_Console
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            using (SampleDatabaseEntities db = new SampleDatabaseEntities())
+            {
+                var query = from v in db.Veicolo
+                            select v;
+
+                foreach (Veicolo veicolo in query)
+                {
+                    Console.WriteLine($"{veicolo.IDVeicolo}, {veicolo.Marca} {veicolo.Modello}");
+                }
+
+            }
+        }
+    }
+}
