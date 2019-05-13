df3468df2e40b9106425e4c1891b5725ec00f8e0
zetanove <supportoinformatico@confimprenditori.it>
2019-05-13 12:54:03 +0200

    codice unsafe

---
+++ Capitolo Appendice B/UnsafeCode/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Runtime.InteropServices;
+
+namespace UnsafeCode
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            NativeMethods.MessageBox(IntPtr.Zero, "test", "test cap", 0);
+           
+
+            unsafe
+            {
+                int i = 123;
+
+                int* pInt1 = &i;
+                int* pInt2;
+                pInt2 = pInt1;
+
+                Console.Write("{0} {1}", *pInt1, *pInt2);
+            }
+        }
+    }
+
+    public unsafe struct Node
+    {
+        public int Value;
+        public Node* Left;
+        public Node* Right;
+    }
+
+    class NativeMethods
+    {
+        [DllImport("user32.dll")]
+        public static extern int MessageBox(IntPtr hWnd, String text, String caption, int options);
+    }
+}
