2fe4aba881197e5538a98460a1ecb8fec0784eac
zetanove <supportoinformatico@confimprenditori.it>
2019-05-14 08:58:25 +0200

    EF Model First

--- Capitolo 16/ModelFirst/Program.cs
+++ Capitolo 16/ModelFirst/Program.cs
@@ -10,6 +10,22 @@ namespace ModelFirst
     {
         static void Main(string[] args)
         {
+            using (var db = new CarModelContainer())
+            {
+                Person persona = new Person();
+                persona.FirstName = "Antonio";
+                persona.LastName = "Pelleriti";
+                db.PersonSet.Add(persona);
+                db.SaveChanges();
+
+                Car car1 = new Car();
+                car1.Targa = "AB123CD";
+                car1.Modello = "Jeep Renegade";
+                car1.Person = persona;
+                db.CarSet.Add(car1);
+                db.SaveChanges();
+            }
         }
+
     }
 }
