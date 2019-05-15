f8f725d58260c40361d79a1ffb92b9956460b38c
zetanove <supportoinformatico@confimprenditori.it>
2019-05-15 15:58:40 +0200

    static local function

--- Capitolo 7/Metodi2/LocalFunction.cs
+++ Capitolo 7/Metodi2/LocalFunction.cs
@@ -9,6 +9,7 @@ namespace Metodi
         internal void Demo()
         {
             CalcolaFattoriali(10);
+            CalcolaFattoriali3(5);
         }
 
         public void CalcolaFattoriali(int max)
@@ -24,9 +25,53 @@ namespace Metodi
                         return num;
                     return num * Fattoriale(num - 1); //moltiplica n per il risultato dello stesso metodo Fattoriale di n-1
                 }
-
             }
+        }
 
+        public void CalcolaFattoriali2(int max)
+        {
+            for (int i = 0; i < max; i++)
+            {
+                Console.WriteLine("fattoriale({0}) = {1}", i, Fattoriale(i));
+
+                static int Fattoriale(int num)
+                {
+                    //Console.WriteLine("Sono all'interno del ciclo {0}", i); // errore, non è possibile usare i  Error CS8421  A static local function cannot contain a reference to 'i'.
+                    return num <=1? num: num * Fattoriale(num - 1);
+                }
+            }
+        }
+
+        public void CalcolaFattoriali3(int max)
+        {
+            for (int i = 0; i < max; i++)
+            {
+                Console.WriteLine("fattoriale({0}) = {1}", i, Fattoriale(i, i));
+
+                static int Fattoriale(int num, int count)
+                {
+                    Console.WriteLine("Sono all'interno del ciclo {0}", count); // errore, non è possibile usare i  Error CS8421  A static local function cannot contain a reference to 'i'.
+                    return num <= 1 ? num : num * Fattoriale(num - 1, count);
+                }
+            }
+        }
+
+        string fn = "name", ln = "surname";
+        void StaticLocalFunction()
+        {
+            var firstName = "Jeetendra";
+            var lastName = "Gund";
+
+            Console.WriteLine("My name is " + GetFullName(firstName, lastName));
+
+            //static string GetFullName() => firstName + ' ' + lastName;
+
+            //static string GetFullName()
+            //{
+            //    return fn + ' '+ ln;
+            //}
+
+            static string GetFullName(string first, string last) => first + ' ' + last;
         }
     }
 }
