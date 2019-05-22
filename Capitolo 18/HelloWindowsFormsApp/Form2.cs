37846c730c0218d96d411e97a17ba9c1a1be31c9
zetanove <supportoinformatico@confimprenditori.it>
2019-05-22 17:50:25 +0200

    esempi pratici

---
+++ Capitolo 18/HelloWindowsFormsApp/Form2.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace HelloWindowsFormsApp
+{
+    public partial class Form2 : Form
+    {
+        public Form2()
+        {
+            InitializeComponent();
+
+        }
+
+        private void Button1_Click(object sender, EventArgs e)
+        {
+            DialogResult result = MessageBox.Show("continuare?", "scegli", MessageBoxButtons.OKCancel, MessageBoxIcon.Question);
+            if (result == System.Windows.Forms.DialogResult.OK)
+            {
+                label1.Text = "Hai cliccato OK";
+            }
+            else label1.Text = "Hai cliccato annulla";
+
+        }
+
+        private void Form2_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
