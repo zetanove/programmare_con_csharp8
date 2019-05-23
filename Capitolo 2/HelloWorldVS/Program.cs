d693de23adcf8e4c7c1708aabe721a64dec08a70
zetanove <supportoinformatico@confimprenditori.it>
2019-05-23 16:20:34 +0200

    esempio hello world rimozione app web capitolo 18

---
+++ Capitolo 2/HelloWorldVS/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace HelloWorldVS
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+
+            if (args != null && args.Length > 0)
+                Console.WriteLine(args[0]);
+        }
+    }
+}
