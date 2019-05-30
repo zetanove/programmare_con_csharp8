241ea2ae81b45308d53f2a41e6c3e7edb465d35d
zetanove <supportoinformatico@confimprenditori.it>
2019-05-30 09:59:09 +0200

    nullable references

---
+++ Capitolo 4/Nullable/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace Nullable
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+#nullable enable
+            string str = null;
+            var test = str.Substring(1);
+#nullable disable
+            string str2 = null;
+        }
+    }
+}
