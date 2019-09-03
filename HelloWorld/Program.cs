7e1defe8fb55db6ef98662053ff75cc46e7ecac2
zetanove <supportoinformatico@confimprenditori.it>
2019-09-03 08:59:43 +0200

    aggiornamento esempi

--- HelloWorld/Program.cs
+++ HelloWorld/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Runtime.InteropServices;
+using System.Xml;
 using static System.Math;
 
 namespace HelloWorld
@@ -19,10 +20,7 @@ namespace HelloWorld
             int[] a = { 0, 1, 2, 3, 4, 5, 6, 7, 8, 9 };
             Console.WriteLine($"{a[i1]}, {a[i2]}"); // "3, 6"
 
-            string s = "";
-
-            Sqrt(123);
-
+            Sqrt(123);           
 
         }
     }
