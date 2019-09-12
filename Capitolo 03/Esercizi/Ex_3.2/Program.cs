60c305733356256d7ea51e6de3e5ee63d9f24116
zetanove <supportoinformatico@confimprenditori.it>
2019-09-12 15:52:13 +0200

    esercizio 3.2

---
+++ Capitolo 03/Esercizi/Ex_3.2/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace Ex3_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string nome = args[0];
+            string cognome = args[1];
+            Console.WriteLine($"Ciao {nome} {cognome}, oggi è il giorno {DateTime.Today:d} e sono le {DateTime.Now:HH:mm:ss}");
+        }
+    }
+}
