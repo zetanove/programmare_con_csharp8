020ad5c1961b0b04db50f7318ac9c5f822153ae1
zetanove <supportoinformatico@confimprenditori.it>
2019-09-12 16:51:18 +0200

    esercizio 3.4

---
+++ Capitolo 03/Esercizi/Ex_3.4/Program.cs
@@ -0,0 +1,22 @@
+﻿/*
+ * Programmare con C# 8 guida completa
+ * Autore: Antonio Pelleriti
+ * Capitolo 3: esercizio 4
+ */
+
+
+using System;
+
+namespace Ex_3._4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Inserisci il tuo nome: ");
+            string nome = Console.ReadLine();
+            Console.WriteLine($"Ciao {nome}, premi un tasto per uscire.");
+            Console.ReadKey();
+        }
+    }
+}
