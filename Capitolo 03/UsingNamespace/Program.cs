3382629b526896a577af77e9c491308501b1e1f4
zetanove <supportoinformatico@confimprenditori.it>
2019-09-25 09:07:05 +0200

    using namespace

---
+++ Capitolo 03/UsingNamespace/Program.cs
@@ -0,0 +1,44 @@
+﻿/*
+ * Programmare con C# 8 guida completa
+ * Autore: Antonio Pelleriti
+ * Capitolo 3: i namespace e using
+ */
+
+using static System.Console;
+using Draw = System.Drawing; //alias di namespace
+
+namespace Using
+{
+    using System;
+    using static Nuovo.Console2;
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Title = "Esempio Console I/O";
+            BackgroundColor = ConsoleColor.White;
+            Clear();
+            ForegroundColor = ConsoleColor.Blue;
+            Write(" Hello World");
+
+            WriteLine("Uso la classe Console2");
+
+            Draw.Point pt=new Draw.Point(); //uso l'alias 
+
+            ReadKey();
+        }
+    }
+}
+
+
+namespace Nuovo
+{
+    public class Console2
+    {
+        public static void WriteLine(string a)
+        {
+            System.Console.WriteLine("Console2.WriteLine(" + a + ")");
+        }
+    }
+}
