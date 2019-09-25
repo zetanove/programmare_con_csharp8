23a73745450bd948772b18db2d96d6a809fa6bbe
zetanove <supportoinformatico@confimprenditori.it>
2019-09-25 09:10:04 +0200

    direttive preprocessore

---
+++ Capitolo 03/DirettivePreprocessore/Program.cs
@@ -0,0 +1,26 @@
+﻿/*
+ * Programmare con C# 8 guida completa
+ * Autore: Antonio Pelleriti
+ * Capitolo 3: direttive preprocessore
+ */
+
+
+using System;
+
+namespace DirettivePreprocessore
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //modifica la configurazione di compilazione per utilizzare i simboli
+#if DEBUG
+            Console.WriteLine("modo debug");
+#elif RELEASE
+            Console.WriteLine("modo release");
+#else
+            Console.WriteLine("debug e release non definite");
+#endif
+        }
+    }
+}
