ee0ebfffc55b28b12e59c158ce62dedd5104df05
zetanove <supportoinformatico@confimprenditori.it>
2019-09-25 16:27:15 +0200

    esercizio 4.2 e 4.3

--- Capitolo 4/Esercizi/Ex_4.2/Program.cs
+++ Capitolo 4/Esercizi/Ex_4.2/Program.cs
@@ -1,4 +1,12 @@
-﻿using System;
+﻿/*
+ * Programmare con C# 8 guida completa
+ * Autore: Antonio Pelleriti
+ * Capitolo 4:
+ * Esercizio 2) Scrivere un’applicazione Console che richieda all’utente l’inserimento di
+ * due numeri e ne stampi somma e prodotto.
+ */
+
+using System;
 
 namespace Ex_4._2
 {
