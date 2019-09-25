684ec80a5c850bfea862211bd2f36efcd1a1c091
zetanove <supportoinformatico@confimprenditori.it>
2019-09-25 09:52:24 +0200

    esercizio 4.2

---
+++ Capitolo 4/Esercizi/Ex_4.2/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace Ex_4._2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Inserisci il numero 1:");
+            int i = int.Parse(Console.ReadLine());
+            Console.WriteLine("Inserisci il numero 2:");
+            int j = int.Parse(Console.ReadLine());
+            int somma = i + j;
+            Console.WriteLine($"{i}+{j}={somma}");
+        }
+    }
+}
