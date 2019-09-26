07899c2125b9d52231686d92576b6d1a4f462409
zetanove <supportoinformatico@confimprenditori.it>
2019-09-26 17:37:16 +0200

    esercizio 5.1

---
+++ Capitolo 5/Esercizi/Ex_5.1/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace Ex_5._1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Inserisci numero 1:");
+            int a = int.Parse(Console.ReadLine());
+            Console.WriteLine("Inserisci numero 2:");
+            int b = int.Parse(Console.ReadLine());
+            Console.WriteLine($"a + b = {a + b}");
+            Console.WriteLine($"a - b = {a - b}");
+            Console.WriteLine($"a * b = {a * b}");
+            Console.WriteLine($"a / b = {(double)a / b}");
+        }
+    }
+}
