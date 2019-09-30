2974b3261cca52a41ad5ac5f7059c1b7302e3605
zetanove <supportoinformatico@confimprenditori.it>
2019-09-30 10:32:48 +0200

    esercizio 5.2

---
+++ Capitolo 5/Esercizi/Ex_5.2/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace Ex_5._2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Inserisci numero 1:");
+            int a = int.Parse(Console.ReadLine());
+            Console.WriteLine("Inserisci numero 2:");
+            int b = int.Parse(Console.ReadLine());
+
+            var and = a & b;
+            var or = a | b;
+            string strAnd = Convert.ToString(and, 2);
+            string strOr = Convert.ToString(or, 2);
+
+            Console.WriteLine($"a & b = {strAnd}");
+            Console.WriteLine($"a | b = {strOr}");
+        }
+    }
+}
