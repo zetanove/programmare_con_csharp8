21a24370ae0fcd43f0aaf019c4bc637daabb5515
zetanove <supportoinformatico@confimprenditori.it>
2019-09-30 17:32:17 +0200

    esercizio 12.2

---
+++ Capitolo 12/Esercizi/Ex_12.2/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Linq;
+
+namespace Ex_12._2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //opzione 1
+            Console.WriteLine("Inserisci una frase");
+            var str = Console.ReadLine();
+
+            var counts = str.GroupBy(c => c) // raggruppa per ogni carattere                                                  
+                     .OrderBy(c => c.Key)   // ordina alfabeticamente
+                                            // converte in dizionario con chiave = carattere, e calore = il conteggio
+                     .ToDictionary(grp => grp.Key, grp => grp.Count());
+
+            foreach(var c in counts)
+            {
+                Console.WriteLine($"{c.Key}: {c.Value}");
+            }
+
+
+
+
+        }
+    }
+}
