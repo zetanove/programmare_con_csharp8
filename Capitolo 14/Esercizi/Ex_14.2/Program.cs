93f855bc8d4ae713a2dad7db3901fa03d2f99d7b
zetanove <supportoinformatico@confimprenditori.it>
2019-10-04 12:24:42 +0200

    esercizi 14.1 e 14.2

---
+++ Capitolo 14/Esercizi/Ex_14.2/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Xml;
+using System.Xml.XPath;
+
+namespace Ex_14._2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            XPathDocument xpDoc = new XPathDocument("studenti.xml");
+            XPathNavigator nav = xpDoc.CreateNavigator();
+            var result=nav.Select("//Classe/Studente");
+
+            foreach(XPathNavigator studente in result)
+            { 
+                Console.WriteLine(studente.InnerXml);
+            }
+        }
+    }
+}
