28cca14ac42264ab143f8dfa7bdca8dd96268c7b
zetanove <supportoinformatico@confimprenditori.it>
2019-10-04 15:42:18 +0200

    esercizio 16.1

---
+++ Capitolo 16/Esercizi/Ex_16.1/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.IO;
+
+namespace Ex_16._1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Inserisci il nome del file da creare:");
+            var filename = Console.ReadLine();
+            if(!filename.EndsWith(".txt"))
+            {
+                filename = filename + ".txt";
+            }
+
+            var file=File.OpenWrite(filename);
+            using StreamWriter sw = new StreamWriter(file) ;
+
+            Console.WriteLine("digita i tasti da salvare sul file:");
+            var k = Console.ReadKey();
+            while(k.Key!= ConsoleKey.X)
+            {
+                sw.WriteLine(k.Key);
+                
+                k = Console.ReadKey();
+            }
+        }
+    }
+}
