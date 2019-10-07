68cf38e0e2744e84ce25537f7672e3f269c75586
zetanove <supportoinformatico@confimprenditori.it>
2019-10-07 15:37:17 +0200

    esempio Visual Studio Code

---
+++ Capitolo 02/HelloVSCode/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace HelloVSCode
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello Visual Studio Code!");
+        }
+    }
+}
